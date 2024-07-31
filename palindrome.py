40ab11da2d974b74f92cc730ed33b36590d26173
Sayandeep Biswas <126375013+Itssayandeep@users.noreply.github.com>
2024-07-31 22:45:15 +0530

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -12,13 +12,13 @@ def number_palindrome(s):
         temp//=10
     return int(s)==ans
 s= input("Enter a sentence/number: ")
-if(s.isalpha()):
-    if sentence_Palindrome(s):
-        print("The sentence is a palindrome.")
-    else:
-        print("The sentence is not a palindrome.")
-else:
+if(s.isalnum()):
     if number_palindrome(s):
+        print( s,"is a palindrome.")
+    else:
+        print(s,"is not a palindrome.")
+else:
+    if sentence_palindrome(s):
         print(s," is a palindrome.")
     else:
         print(s,"is not a palindrome.")
